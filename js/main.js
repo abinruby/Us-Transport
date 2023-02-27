c75101386d48216a06fbc5091d374b8a221a5a8e
Abin K Ruby <abinruby51@gmail.com>
2023-02-27 21:42:23 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ const preloaderWrapper=document.querySelector('.preloader-wrapper');
 window.addEventListener('load', function(){
     preloaderWrapper.classList.add('fade-out-animation')
 })
+
+function clickButton() {
+    document.getElementById("nextButton").click();
+  }
+  
+  setInterval(clickButton, 3000);
